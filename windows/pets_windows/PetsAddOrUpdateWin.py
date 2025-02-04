567d8b150731baa7796b5751ddfbe3500787b87f
maryjane338 <glitch.tv.sav@gmail.com>
2025-02-04 10:35:12 +0900

    Add files via upload

---
+++ windows/pets_windows/PetsAddOrUpdateWin.py
@@ -0,0 +1,92 @@
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import *
+from database import SessionLocal
+from services.service import PetService
+
+
+# from services.book_service import BookService
+
+
+class PetsAddOrUpdateWin(QWidget):
+    def __init__(self, pets_win, pet=None):
+        super().__init__()
+        self.pets_win = pets_win
+        self.pet = pet
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Заказ')
+        self.setGeometry(50, 100, 300, 300)
+        self.setFixedSize(self.width(), self.height())
+        self.setWindowIcon(QIcon('logo_pictures/window_icon.png'))
+
+        name_label = QLabel('Введите имя питомца:')
+        self.name_input = QLineEdit()
+        pet_species_label = QLabel('Введите вид питомца:')
+        self.pet_species_input = QLineEdit()
+        age_label = QLabel('Введите возраст питомца:')
+        self.age_input = QLineEdit()
+        weight_label = QLabel('Введите вес питомца:')
+        self.weight_input = QLineEdit()
+        self.save_btn = QPushButton('Сохранить')
+        self.id = QLineEdit()
+        self.save_btn.clicked.connect(self.save_pet)
+
+        if self.pet:
+            self.name_input.setText(self.pet['pet_name'])
+            self.pet_species_input.setText(self.pet['animal_species'])
+            self.age_input.setText(self.pet['age'])
+            self.weight_input.setText(self.pet['weight'])
+            self.id.setText(self.pet['id_pet'])
+
+        main_l = QVBoxLayout()
+        main_l.addWidget(name_label)
+        main_l.addWidget(self.name_input)
+        main_l.addWidget(pet_species_label)
+        main_l.addWidget(self.pet_species_input)
+        main_l.addWidget(age_label)
+        main_l.addWidget(self.age_input)
+        main_l.addWidget(weight_label)
+        main_l.addWidget(self.weight_input)
+        main_l.addStretch()
+        main_l.addWidget(self.save_btn)
+        self.setLayout(main_l)
+
+    def save_pet(self):
+        db = SessionLocal()
+        pet_service = PetService(db)
+
+        try:
+            int(self.age_input.text())
+            int(self.weight_input.text())
+
+            if self.name_input.text() == '' or self.pet_species_input.text() == '' or self.age_input.text() == ''\
+                    or self.weight_input.text() == '':
+                QMessageBox.information(self, 'Информация',
+                                        'Вы не заполнили все поля или заполнили их некорректно.')
+            else:
+                if self.id.text():
+                    pet_service.update_pet(
+                        id_pet=self.id.text(),
+                        pet_name=self.name_input.text(),
+                        animal_species=self.pet_species_input.text(),
+                        age=self.age_input.text(),
+                        weight=self.weight_input.text(),
+                    )
+                else:
+                    pet_service.add_pet(
+                        pet_name=self.name_input.text(),
+                        animal_species=self.pet_species_input.text(),
+                        age=self.age_input.text(),
+                        weight=self.weight_input.text(),
+                    )
+                QMessageBox.information(self, 'Информация', 'Книга успешно сохранена!')
+                self.close()
+                self.pets_win.model.clear()
+                self.pets_win.model.setHorizontalHeaderLabels(['id_order', 'user_name', 'Книга', 'Адрес доставки',
+                                                               'Способ оплаты', 'Дата доставки'])
+                self.pets_win.load_orders()
+
+        except ValueError:
+            QMessageBox.information(self, 'Информация',
+                                    'Вы не заполнили все поля или заполнили их некорректно.')
--- windows/pets_windows/PetsWin.py
+++ windows/pets_windows/PetsWin.py
@@ -1,63 +1,109 @@
-from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QIcon, QStandardItemModel, QStandardItem
+from PyQt6.QtWidgets import *
 from database import SessionLocal, init_db
-from services.service import ShelterService
+from services.service import PetService
+from windows.pets_windows.PetsAddOrUpdateWin import PetsAddOrUpdateWin
 
 
-class SheltersWin(QWidget):
+# from services.book_service import OrderService
+# from windows.admin.UpdateOrderWin import UpdateOrderWin
+
+
+class UnhomePetsWin(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Дом для животных')
-        self.setGeometry(1150, 600, 400, 400)
+        self.setWindowTitle('Заказы')
+        self.setGeometry(50, 100, 550, 400)
         self.setFixedSize(self.width(), self.height())
         self.setWindowIcon(QIcon('logo_pictures/window_icon.png'))
 
+        self.update_btn = QPushButton('Изменить')
+        self.update_btn.clicked.connect(self.show_update_win)
+        self.add_btn = QPushButton('Добавить')
+        self.add_btn.clicked.connect(self.show_add_win)
+        self.delete_btn = QPushButton('Удалить')
+        self.delete_btn.clicked.connect(self.delete_order)
 
         init_db()
         db = SessionLocal()
 
-        self.shelter_service = ShelterService(db)
+        self.pet_service = PetService(db)
 
         self.view = QTableView()
         self.model = QStandardItemModel()
-        self.model.setHorizontalHeaderLabels(['id_shelter', 'pet_name', 'parent_name'])
+        self.model.setHorizontalHeaderLabels(['id_pet', 'pet_name', 'animal_species', 'age', 'weight', 'home_status'])
         self.view.setModel(self.model)
 
-        self.load_users()
+        self.load_orders()
 
         main_l = QVBoxLayout()
         v_l = QHBoxLayout()
         main_l.addLayout(v_l)
+        v_l.addWidget(self.update_btn)
+        v_l.addWidget(self.add_btn)
+        v_l.addWidget(self.delete_btn)
         main_l.addWidget(self.view)
         self.setLayout(main_l)
 
-    def load_users(self):
-        shelters = self.shelter_service.get_all_shelters()
-        for shelter in shelters:
-            row = [QStandardItem(field) for field in shelter]
+    def load_orders(self):
+        orders = self.pet_service.get_all_pet()
+        for order in orders:
+            row = [QStandardItem(field) for field in order]
             self.model.appendRow(row)
 
-    def delete_user(self):
+    def show_update_win(self):
+        indexes = self.view.selectionModel().selectedRows()
+        if indexes:
+            row = indexes[0].row()
+            selected_pet = {
+                'id_pet': self.model.item(row, 0).text(),
+                'pet_name': self.model.item(row, 1).text(),
+                'animal_species': self.model.item(row, 2).text(),
+                'age': self.model.item(row, 3).text(),
+                'weight': self.model.item(row, 4).text(),
+                'home_status': self.model.item(row, 5).text(),
+            }
+            self.pets_add_or_update_win = PetsAddOrUpdateWin(self, selected_pet)
+            self.pets_add_or_update_win.show()
+        else:
+            QMessageBox.information(self, 'Информация', 'Для выбора записи, нажмите на её номер в таблице',
+                                    QMessageBox.StandardButton.Ok)
+
+    def show_add_win(self):
+        self.pets_add_or_update_win = PetsAddOrUpdateWin(self)
+        self.pets_add_or_update_win.show()
+
+    def delete_order(self):
         indexes = self.view.selectionModel().selectedRows()
         if indexes:
             row = indexes[0].row()
             dialog = QMessageBox()
-            dialog.setGeometry(1200, 775, 400, 400)
+            dialog.setGeometry(275, 450, 750, 400)
             dialog.setWindowTitle("Подтверждение")
-            dialog.setText(f"Вы уверены, что хотите удалить пользователя?")
+            dialog.setText(f"Вы уверены, что хотите удалить заказ?")
             dialog.setIcon(QMessageBox.Icon.Warning)
             dialog.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
             user_response = dialog.exec()
             if user_response == QMessageBox.StandardButton.Yes:
-                self.client_service.delete_client(self.model.item(row, 0).text())
-                QMessageBox.information(self, "Инфо", 'Запись удалена')
+                self.order_service.delete_order(self.model.item(row, 0).text())
+                QMessageBox.information(self, "Инфо", 'Заказ удалена')
                 self.model.clear()
-                self.model.setHorizontalHeaderLabels(['id', 'Автор', 'Название', 'Картинка', 'Цена'])
-                self.load_users()
+                self.model.setHorizontalHeaderLabels(
+                    ['id_order', 'user_name', 'Книга', 'Адрес доставки', 'Способ оплаты',
+                     'Дата доставки'])
+                self.load_orders()
         else:
-            QMessageBox.information(self, 'Информация',
-                                    'Для удаления пользователя, нажмите на его номер в таблице',
+            QMessageBox.information(self, 'Информация', 'Для удаления заказа, нажмите на его номер в таблице',
                                     QMessageBox.StandardButton.Ok)
+
+    def closeEvent(self, event):
+        try:
+            if self.update_order_win.isVisible():
+                self.update_order_win.close()
+        except AttributeError:
+            pass
+        finally:
+            event.accept()

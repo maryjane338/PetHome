ff697d965796687876ea8e5c952e2cf2b1b65e05
maryjane338 <glitch.tv.sav@gmail.com>
2025-03-04 13:47:15 +0900

    Add files via upload

---
+++ windows/events_windows/EventAddOrUpdateWin.py
@@ -0,0 +1,79 @@
+from datetime import date
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import *
+from database import SessionLocal
+from services.service import EventService
+
+
+class EventAddOrUpdateWin(QWidget):
+    def __init__(self, events_win, event=None):
+        super().__init__()
+        self.events_win = events_win
+        self.event = event
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Заказ')
+        self.setGeometry(400, 600, 300, 200)
+        self.setFixedSize(self.width(), self.height())
+        self.setWindowIcon(QIcon('logo_pictures/window_icon.png'))
+
+        name_label = QLabel('Введите название события:')
+        self.name_input = QLineEdit()
+        date_label = QLabel('Введите дату мероприятия:')
+        self.date_input = QLineEdit()
+        self.save_btn = QPushButton('Сохранить')
+        self.id = QLineEdit()
+        self.save_btn.clicked.connect(self.save_event)
+
+        if self.event:
+            self.name_input.setText(self.event['name'])
+            self.date_input.setText(self.event['date'])
+            self.id.setText(self.event['id'])
+
+        main_l = QVBoxLayout()
+        main_l.addWidget(name_label)
+        main_l.addWidget(self.name_input)
+        main_l.addWidget(date_label)
+        main_l.addWidget(self.date_input)
+        main_l.addStretch()
+        main_l.addWidget(self.save_btn)
+        self.setLayout(main_l)
+
+    def save_event(self):
+        db = SessionLocal()
+        event_service = EventService(db)
+
+        try:
+            date_text = self.date_input.text()
+            date_list = list(date_text.split('-'))
+            print(date_list)
+            int(date_list[0])
+            int(date_list[1])
+            int(date_list[2])
+
+            if self.name_input.text() == '' or self.date_input.text() == '' or len(date_list[0]) != 4\
+                    or len(date_list[1]) != 2 or len(date_list[2]) != 2:
+                QMessageBox.information(self, 'Информация',
+                                        'Вы не заполнили все поля или заполнили их некорректно.')
+            else:
+                if self.id.text():
+                    event_service.update_event(
+                        id_event=self.id.text(),
+                        event_name=self.name_input.text(),
+                        event_date=date(int(date_list[0]), int(date_list[1]), int(date_list[2]))
+                    )
+                else:
+                    event_service.add_event(
+                        event_name=self.name_input.text(),
+                        event_date=date(int(date_list[0]), int(date_list[1]), int(date_list[2]))
+                    )
+                QMessageBox.information(self, 'Информация', 'Книга успешно сохранена!')
+                self.close()
+                self.events_win.model.clear()
+                self.events_win.model.setHorizontalHeaderLabels(['id_event', 'event_name', 'event_date'])
+                self.events_win.load_users()
+
+        except ValueError:
+            QMessageBox.information(self, 'Информация',
+                                    'Вы не заполнили все поля или заполнили их некорректно.')
--- windows/events_windows/EventsWin.py
+++ windows/events_windows/EventsWin.py
@@ -2,6 +2,7 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QIcon, QStandardItemModel, QStandardItem
 from database import SessionLocal, init_db
 from services.service import EventService
+from windows.events_windows.EventAddOrUpdateWin import EventAddOrUpdateWin
 
 
 class EventsWin(QWidget):
@@ -14,6 +15,13 @@ class EventsWin(QWidget):
         self.setGeometry(50, 600, 350, 400)
         self.setFixedSize(self.width(), self.height())
         self.setWindowIcon(QIcon('logo_pictures/window_icon.png'))
+
+        self.add_btn = QPushButton('Добавить')
+        self.add_btn.clicked.connect(self.show_event_add_win)
+        self.update_btn = QPushButton('Изменить')
+        self.update_btn.clicked.connect(self.show_event_update_win)
+        self.delete_btn = QPushButton('Удалить')
+        self.delete_btn.clicked.connect(self.delete_event)
 
         init_db()
         db = SessionLocal()
@@ -30,6 +38,9 @@ class EventsWin(QWidget):
         main_l = QVBoxLayout()
         v_l = QHBoxLayout()
         main_l.addLayout(v_l)
+        v_l.addWidget(self.add_btn)
+        v_l.addWidget(self.update_btn)
+        v_l.addWidget(self.delete_btn)
         main_l.addWidget(self.view)
         self.setLayout(main_l)
 
@@ -38,3 +49,52 @@ class EventsWin(QWidget):
         for event in events:
             row = [QStandardItem(field) for field in event]
             self.model.appendRow(row)
+
+    def show_event_update_win(self):
+        indexes = self.view.selectionModel().selectedRows()
+        if indexes:
+            row = indexes[0].row()
+            selected_event = {
+                'id': self.model.item(row, 0).text(),
+                'name': self.model.item(row, 1).text(),
+                'date': self.model.item(row, 2).text()
+            }
+            self.event_add_or_update_win = EventAddOrUpdateWin(self, selected_event)
+            self.event_add_or_update_win.show()
+        else:
+            QMessageBox.information(self, 'Информация', 'Для выбора записи, нажмите на её номер в таблице',
+            QMessageBox.StandardButton.Ok)
+
+    def show_event_add_win(self):
+        self.event_add_or_update_win = EventAddOrUpdateWin(self)
+        self.event_add_or_update_win.show()
+
+    def delete_event(self):
+        indexes = self.view.selectionModel().selectedRows()
+        if indexes:
+            row = indexes[0].row()
+            dialog = QMessageBox()
+            dialog.setGeometry(50, 650, 350, 400)
+            dialog.setWindowTitle("Подтверждение")
+            dialog.setText(f"Вы уверены, что хотите удалить запись?")
+            dialog.setIcon(QMessageBox.Icon.Warning)
+            dialog.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+            user_response = dialog.exec()
+            if user_response == QMessageBox.StandardButton.Yes:
+                self.event_service.delete_event(self.model.item(row, 0).text())
+                QMessageBox.information(self, "Инфо", 'Запись удалена')
+                self.model.clear()
+                self.model.setHorizontalHeaderLabels(['id_event', 'event_name', 'event_date'])
+                self.load_users()
+        else:
+            QMessageBox.information(self, 'Информация', 'Для удаления записи, нажмите на её номер в таблице',
+            QMessageBox.StandardButton.Ok)
+
+    def closeEvent(self, event):
+        try:
+            if self.event_add_or_update_win.isVisible():
+                self.event_add_or_update_win.close()
+        except AttributeError:
+            pass
+        finally:
+            event.accept()
